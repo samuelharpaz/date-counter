7ed1fe4601146c86f418b0ffc727dd13ab3863ab
Samuel Harpaz <sharpaz24@gmail.com>
2025-07-11 14:03:00 -0400

    add range slider functionality, input field and functionality, and reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  const handleChangeStep = function (e) {
+    setStep(+e.target.value);
+  };
+
+  const handleReset = function () {
+    setCount(0);
+    setStep(1);
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="range" min={1} max={10} value={step} onChange={handleChangeStep} />
+        <span>Step: {step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount(c => c - step)}>-</button>
+        <input type="text" value={count} onChange={e => setCount(+e.target.value)} />
+        <button onClick={() => setCount(c => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0 ? 'Today is ' : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {(count !== 0 || step !== 1) && <button onClick={handleReset}>Reset</button>}
+    </div>
+  );
+}
